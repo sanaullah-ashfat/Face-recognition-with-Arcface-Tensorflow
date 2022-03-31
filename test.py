3089d2830b0ea59118944708698b7a78b005761c
Sanaullah <41291499+sanaullah-ashfat@users.noreply.github.com>
2022-03-31 11:45:25 +0600

    update with clustering data

--- test.py
+++ test.py
@@ -48,3 +48,76 @@ path ="/home/cpsd/Downloads/Images/"
 
 #train =emb.save_multiple_embed(path)
 
+
+def extract_data(self,path,out_path):
+
+    names = []
+    emb=[]
+    embeddings=[]
+    img_list = glob.glob(path+"/*")
+    print(img_list)
+
+    for i in range(len(img_list)):
+        image = img_list[i]
+        image_name= image.split(".")[-1]
+        img_name = image.split("/")[-1].split(".")[0]
+        print(img_name)
+        image = cv2.imread(image)
+        image = cv2.resize(image,(512,512))
+        try:
+            image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+            face, bbox,_ = self.detector.get_cropped_face(image)
+            print(len(face))
+            image= face[0]
+            # print(bbox,bbox)
+            cv2.imwrite("./image.jpg",image)
+            # cv2.imwrite("./images.jpg",face[1])
+            k, idx = self.recognition(image,bbox)
+            print("*********************************************",idx)
+            print("*********************************************",k)
+            if k == "Known":
+                if not os.path.isdir("/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(idx)):
+                    os.mkdir("/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(idx))
+                    # print("---------------------------------------------",path_out)
+                    path_out ="/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(idx)+"/"
+                    cv2.imwrite(path_out+str(img_name)+".png",image )#cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+                    #return "WOW The system work well"
+                else:
+                    path_out ="/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(idx)+"/"
+                    cv2.imwrite(path_out+str(img_name)+".png",image )
+                    #return "WOW The system work well"
+            else:
+
+                images = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = images.astype(np.float32) / 255.
+                #mirror = image.reshape(112,112,3)
+                mirror= cv2.flip(image, 1)
+                mirror= mirror.astype(np.float32) / 255.
+                if len(image.shape) == 3:
+                    image = np.expand_dims(image, 0)
+                    mirror= np.expand_dims(mirror, 0)
+                image = l2_norm(self.model(image))
+                mirror = l2_norm(self.model(mirror))
+                ebd = np.mean([image, mirror],axis=0)
+                emb.append(ebd)
+                names.append(img_name)
+                embd = np.asarray(emb)
+                nam = np.array(names)
+                embds_dict = dict(zip(nam, embd))
+                with open("./project/information/embdding.pkl", "wb") as fi:
+                    bin_obj = pickle.dumps(embds_dict)
+                    fi.write(bin_obj)
+                if not os.path.isdir("/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(img_name)):
+                    os.mkdir("/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(img_name))
+                    path_out = "/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(img_name)+"/"
+                    print("######################################",img_name,path_out)
+                    cv2.imwrite(path_out+str(img_name)+".png", images)
+                else:
+                    path_out = "/home/sanaullah/Documents/facebank/micro_service_update/folder/"+str(img_name)+"/"
+                    print("######################################",img_name,path_out)
+                    cv2.imwrite(path_out+str(img_name)+".png", images)
+
+                #return "sucessfull"
+        except Exception as e:
+            print(e)
+
